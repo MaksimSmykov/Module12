d88e781d3b70c4d7676d9a88ca2e9999b3949fd5
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-12-14 18:41:22 +0300

    Update suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,9 +1,9 @@
 import unittest
-import tests_12_2
+import tests_12_3
 
 rt_Test = unittest.TestSuite()
-rt_Test.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_2.RunnerTest))
-rt_Test.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_2.TournamentTest))
+rt_Test.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.RunnerTest))
+rt_Test.addTest(unittest.TestLoader().loadTestsFromTestCase(tests_12_3.TournamentTest))
 
 runner = unittest.TextTestRunner(verbosity=2)
 runner.run(rt_Test)
